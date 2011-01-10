cef9440be07f2f80fca8ef55291b5244ef3bec17
Brett Bim <osiris43@gmail.com>
2011-01-10 10:24:31 -0600

    Chapter 4 exercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+
+  # Return the logo as home page
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 end
