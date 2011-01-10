1ef114b4735ac633c752b2b3ff30bc244b24ee1e
Brett Bim <osiris43@gmail.com>
2011-01-09 18:22:12 -0600

    stylesheets and chapter 4

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  before(:each) do
+    @title = "Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -12,7 +15,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                                    :content => "Ruby on Rails Tutorial Sample App | Home")
+                                    :content => @title + " | Home")
     end
 
   end
@@ -26,7 +29,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                                    :content => "Ruby on Rails Tutorial Sample App | Contact")
+                                    :content => @title + " | Contact")
     end
   end
 
@@ -39,7 +42,20 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                                    :content => "Ruby on Rails Tutorial Sample App | About")
+                                    :content => @title + " | About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                                    :content => @title + " | Help")
     end
   end
 end
